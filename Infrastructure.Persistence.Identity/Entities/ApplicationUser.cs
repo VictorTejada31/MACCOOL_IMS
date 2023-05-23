448da25730f89fec1a0d94bc077405f4fd561f0b
juanetes02 <131911600+juanetes02@users.noreply.github.com>
2023-05-23 13:41:19 -0400

    ApplicationUser Added

---
+++ Infrastructure.Persistence.Identity/Entities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IdCard { get; set; }
+        public string CretedBy { get; set; }
+        public bool isOnline { get; set; }
+        public DateTime LastConnection { get; set; }
+
+    }
+}
