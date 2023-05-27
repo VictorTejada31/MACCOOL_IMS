df94d651e8b4dc8bf04ebdf09cbd21c984a004d5
juanetes02 <131911600+juanetes02@users.noreply.github.com>
2023-05-27 09:09:52 -0400

    MailSetting class added to Domain/Settings.

---
+++ Core.Domain/Settings/MailSetting.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Domain.Settings
+{
+    public class MailSetting
+    {
+        public string EmailFrom { get; set; }
+        public string SmptUser { get; set; }
+        public string SmptPass { get; set; }
+        public int SmptPort { get; set; }
+        public string SmptHost { get; set; }
+        public string DisplayName { get; set; }
+
+    }
+}
